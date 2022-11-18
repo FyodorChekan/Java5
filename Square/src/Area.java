6e37d819ef607a37c661fab7dd57f8e06b719989
Fyodor Chekan <chekan.fyod@yandex.ru>
2022-11-18 15:45:48 +0300

    First task

--- Square/src/Area.java
+++ Square/src/Area.java
@@ -1,2 +1,3 @@
 public interface Area {
+    public double calculateArea();
 }
---
+++ Square/src/Square.java
@@ -0,0 +1,12 @@
+public class Square implements Area{
+    private double side;
+
+    public Square(double side) {
+        this.side = side;
+    }
+
+    @Override
+    public double calculateArea() {
+        return Math.pow(side, 2);
+    }
+}
